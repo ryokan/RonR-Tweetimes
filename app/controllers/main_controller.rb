f253c82f97ba6e3297e9d14411a778dfa074470a
Yoshihiro Ueda <ueda@iMac.local>
2012-03-24 16:33:00 +0900

    change

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,7 @@ class MainController < ApplicationController
     @poster = Poster.new
     #logs = Log.find :all, :conditions => ["created_at < ?", Time.now - 30*24*60*60 ]
 	#logs = Log.order("created_on DESC").limit(1000).all
-	logs = Log.find :all, :order => "created_at DESC", :limit => 500
+	logs = Log.find :order => "created_at desc", :limit => 500
     posters = []
     logs.each {|log| posters = posters | [log.poster]}
     @posters = posters.map {|poster|
