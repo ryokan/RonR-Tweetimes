a88312e82590955931bc025f7ba68d31c1c98e52
Yoshihiro Ueda <ueda@iMac.local>
2012-03-24 14:42:26 +0900

    change

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,8 @@ class MainController < ApplicationController
 	def index
     @url_linktext = "login"
     @poster = Poster.new
-    # logs = Log.find :all, :conditions => ["created_at < ?", Time.now - 30*24*60*60 ]
-	logs = Log.find :all
+    logs = Log.find :all, :conditions => ["created_at <= ?", Time.now - 30*24*60*60 ]
+	# logs = Log.find :all
     posters = []
     logs.each {|log| posters = posters | [log.poster]}
     @posters = posters.map {|poster|
