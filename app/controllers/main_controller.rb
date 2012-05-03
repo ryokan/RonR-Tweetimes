ff82b1749a10b23b4888a6ceeab6a5a0428acdee
Yoshihiro Ueda <ueda@iMac.local>
2012-05-03 11:06:32 +0900

    comment

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,6 +5,7 @@ class MainController < ApplicationController
 #    logs = Log.find :all, :conditions => ["created_at > ?", Time.now - 7*24*60*60 ]
     logs = Log.find :all, :order => 'created_at desc', :limit => 1000
     posters = []
+    
 #    logs.each {|log| posters = posters | [log.poster]}
 #    logs.each {|log| posters << log.poster}
 #    posters.uniq!
@@ -18,21 +19,21 @@ class MainController < ApplicationController
 #    @posters = @posters[0..9]
     
     
-    
     logs.each {|log| 
       poster = log.poster
-      if p = posters.assoc(poster) 
+      if p = posters.assoc(poster.id) 
         p[1]++
       else
-        posters << [poster, 1]
+        posters << [poster.id, 1]
       end
       }
     posters.sort!{|b,a| a[1] - b[1]}
-    @posters = posters[0..9].map {|poster|
-      {:id => poster[0].id, 
-	  :query => poster[0].query, 
-	  :count => poster[1], 
-	  :created_at => poster[0].created_at}
+    @posters = posters[0..9].map {|p|
+      poster = Poster.find(p[0])
+      {:id => p[0], 
+	  :query => poster.query, 
+	  :count => p[1], 
+	  :created_at => poster.created_at}
     }
    
     @recent = Poster.find :all, :order => 'updated_at desc', :limit => 10
